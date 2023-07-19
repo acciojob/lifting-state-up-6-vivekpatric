960eb33e4dd42b164da2791b8f9c101751e1d797
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-19 15:07:37 +0000

    result

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,52 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {"title":"Learn React"},
+    {"title":"Build a React App"},
+    {"title":"Deploy the React app"},
+  ]);
+
+  const handleCompleteTodo = (index) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return {
+          ...todo,
+          completed: true,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <h1>Parent component</h1>
+      <ChildComponent todos={todos} handleCompleteTodo={handleCompleteTodo} />
     </div>
-  )
+  );
+}
+
+const ChildComponent = ({ todos, handleCompleteTodo }) => {
+  return (
+    <div id="child">
+      <h2>Child Component</h2>
+          {
+            todos.map((todo, index) => (
+            <li key={index}>
+              {todo.title}
+              {!todo.completed && (
+                <button onClick={() => handleCompleteTodo(index)}>Complete</button>
+              )}
+            </li>
+          ))
+          }
+    </div>
+  );
 }
 
 export default App
